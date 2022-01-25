658ee652f2be37798789e6bccd311b6cdc665cec
DarkyDieL <darkydiel@gmail.com>
2022-01-25 20:49:08 -0300

    Orientando a objetos

--- Projectos/clases.py
+++ Projectos/clases.py
@@ -5,22 +5,32 @@ Created on Sat Jan  8 18:16:25 2022
 @author: DieL
 """
 import pandas as pd
-class _Item():
-
-    def __init__(self, nombre, volumen, precio_estimado, precio_de_mercado, ) -> None:
-        
-        self.nombre = nombre
-        self.volumen = volumen
-        self.precio_estimado = precio_estimado
-        self.precio_de_mercado = precio_de_mercado
-
-class Minerales(_Item):
-
-    def __init__(self, nombre, volumen, precio_estimado, precio_de_mercado, tiempo_de_minado, ) -> None:
-        self.tiempo_de_minado = tiempo_de_minado
-        super().__init__(nombre,  
-            volumen, 
-            precio_estimado, 
-            precio_de_mercado,) 
 
 
+
+
+class _M():
+    
+    def __init__(self) -> None:
+        self.link_m = "link carpeta M / "
+
+
+class M_link(_M):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.link_M_link = self.link_m + "link de la carpeta M_link"
+        self.ruta = self.link_m + self.link_M_link
+
+if __name__ == '__main__':
+
+    m = M_link()
+    n = _M()
+    
+    def imprimir(m):
+        print(f" El atriburo ruta de M: {m.link_m}")    
+        print(f" El atriburo ruta de M_link: {m.link_M_link}")
+        print(f" El atriburo ruta completo: {m.ruta}")
+
+    imprimir(m)
+    imprimir(n)
--- Projectos/scraping.py
+++ Projectos/scraping.py
@@ -9,7 +9,6 @@ import random
 from time import sleep
 from selenium import webdriver
 import pandas as pd
-from sklearn.covariance import ledoit_wolf_shrinkage
 
 
 
@@ -68,31 +67,28 @@ if __name__ == '__main__':
     sleep(random.uniform(5.0, 10.0))
     
     expandir_lista()
+    
+    
     vespel_link = '/html/body/div/div[2]/div[1]/div/div/div[1]/div[2]/ul/li[6]/div/ul/li[2]/div/ul/li[9]/div/ul/li[5]/div/ul/li[19]/div/ul/li[9]/h5/a'
     vespel = driver.find_element_by_xpath(vespel_link)
+    vespel_nombre = driver.find_element_by_xpath(vespel_link).text
     vespel.click()
+    
     vespel_link_buy = '/html/body/div/div[2]/div[2]/div/div[2]/ul/li[2]/h4/a'
     vespel = driver.find_element_by_xpath(vespel_link_buy)
     vespel.click()
-
-
-    print("\n")
-    print("Vespel:")
-    print(vespel)
-    print("...")
-    
-
+    print(f"..........................\nEl nombre almacenado en vespel_nombre es: {vespel_nombre} \n..........................")
 
     sleep(random.uniform(2.0, 3.0))
 
     columnas = ["Precio", "Ubicación"]
     lista = pd.DataFrame(columns=columnas) #/html/body/div/div[2]/div[2]/div/div[2]/div/div[2]/table/tbody/tr[1]
+    
     precio = driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/div/div[2]/div/div[2]/table/tbody/tr[1]/td[3]').text
     ubicación = driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/div/div[2]/div/div[2]/table/tbody/tr[1]/td[4]/span[1]').text
+    
     print(f"El precio: {precio} y la ubicación: {ubicación}")
     lista = lista.append({'Precio' : precio , 'Ubicación' : ubicación} , ignore_index=True)
 
-    print("\n\n")
-    print(lista)
-    print("\n\n")
+    
 
